705110e50a47f79651c5a67053922e834e726a53
Samuel Hart <sam.hart@darkwolfsolutions.com>
2019-04-23 12:03:49 -0500

    More refinement

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,33 +13,15 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
-  selectedHeroes: Map<Hero["id"], Hero>;
   
-
   constructor(private heroService: HeroService, private messageService: MessageService) {}
   
   ngOnInit() {
     this.getHeroes();
-    if(this.selectedHeroes === undefined)
-      this.selectedHeroes = new Map<Hero["id"], Hero>();
   }
-  
-  onSelect(hero: Hero, source?: string): void {
-    let sourceval = (source === undefined) ? "." : " from " + source +".";
-    
-    this.selectedHero = hero;
-    this.selectedHeroes.set(this.selectedHero.id, this.selectedHero);
-    this.messageService.add("Selected "+ this.selectedHero.name + sourceval);
-  }
- 
+
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
-  getSelectedHeroes(): Hero[] {
-    return Array.from(this.selectedHeroes.values());
-  }
-
 }
 
